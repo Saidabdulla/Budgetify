12404e1ce5aeb5c1920d675af68328fef170f381
Saidabdulla <abdullamasharipov11@gmail.com>
2022-03-11 23:31:51 +0500

    Last changes

--- server.js
+++ server.js
@@ -3,12 +3,14 @@ const app = express();
 
 const accountRoutes = require("./routes/account");
 const transactionRoutes = require("./routes/transaction");
+const categoryRoutes = require("./routes/category");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/account", accountRoutes);
 app.use("/transaction", transactionRoutes);
+app.use("/category", categoryRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server working on ${PORT} - port.`));
