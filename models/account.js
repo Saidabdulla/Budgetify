82f3cffa577bbb23e1ad28d7225e55b45a1f3cc1
Saidabdulla <abdullamasharipov11@gmail.com>
2022-03-19 02:00:11 +0500

    Account schema

---
+++ models/account.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const { Schema } = mongoose;
+
+const User = require("./user");
+
+const accountSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    currency: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    user_id: {
+        type: Schema.Types.ObjectId,
+        ref: User,
+        required: true,
+        trim: true,
+    },
+});
+
+module.exports = mongoose.model("account", accountSchema);
--- models/user.js
+++ models/user.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
+const { Schema } = mongoose;
 
-const userSchema = new mongoose.Schema({
+const userSchema = new Schema({
     firstName: {
         type: String,
         required: true,
