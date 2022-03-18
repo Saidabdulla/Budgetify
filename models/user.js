4adb9535ebfad1ff9b4673338efed8a6fea52360
Saidabdulla <abdullamasharipov11@gmail.com>
2022-03-19 01:52:18 +0500

    User schema

---
+++ models/user.js
@@ -0,0 +1,48 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    age: {
+        type: Number,
+        required: true,
+        max: 100,
+        min: 0,
+    },
+    country: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    currentLocation: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    gender: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+});
+
+module.exports = mongoose.model("user", userSchema);
