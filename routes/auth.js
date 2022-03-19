6036fac2199ca4ab9e9a7c7fa1d4d002074ba3f1
Saidabdulla <abdullamasharipov11@gmail.com>
2022-03-19 22:43:30 +0500

    Login endpoint with mongodb

--- routes/auth.js
+++ routes/auth.js
@@ -3,8 +3,6 @@ const router = express.Router();
 
 const { register, login } = require("../controllers/authController");
 
-router.post("/register", register);
-
 router.post("/login", login);
 
 module.exports = router;
