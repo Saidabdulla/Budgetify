713a36b29182ec9d1d873c2c2a477feb0ee86649
Saidabdulla <abdullamasharipov11@gmail.com>
2022-04-05 23:06:41 +0500

    app module

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module'; // CLI imports AppRoutingModule
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { AuthModule } from './auth/auth.module';
 import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
     declarations: [AppComponent, NotFoundComponent],
-    imports: [BrowserModule, AppRoutingModule],
+    imports: [BrowserModule, AuthModule, SharedModule, AppRoutingModule],
     providers: [],
     bootstrap: [AppComponent],
 })
