e14662ff126fe4760858c46d435f7ff67dd960cb
Saidabdulla <abdullamasharipov11@gmail.com>
2022-04-23 12:25:28 +0500

    Account controller

--- server/controllers/accountController.js
+++ server/controllers/accountController.js
@@ -1,78 +1,102 @@
 const Account = require("../models/account");
 
 exports.getAccounts = async (req, res) => {
-    const accounts = await Account.find({ user_id: req.user._id });
+    try {
+        const accounts = await Account.find({ user_id: req.user._id }).populate(
+            "currency_id"
+        );
 
-    res.status(200).json(accounts);
+        res.status(200).json(accounts);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 exports.getAccountById = async (req, res) => {
-    const account = await Account.findById(req.params.id);
+    try {
+        const account = await Account.findById(req.params.id).populate(
+            "currency_id"
+        );
 
-    res.status(200).json(account);
+        res.status(200).json(account);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 exports.addAccount = async (req, res) => {
-    const checkAcc = await Account.findOne({
-        $and: [
-            { name: req.body.name.toLowerCase() },
-            {
-                currency: req.body.currency.toLowerCase(),
-            },
-        ],
-    });
+    try {
+        const checkAcc = await Account.findOne({
+            $and: [
+                { name: req.body.name.toLowerCase() },
+                {
+                    currency: req.body.currency.toLowerCase(),
+                },
+            ],
+        });
 
-    if (checkAcc) {
-        return res.json("Account exist!");
+        if (checkAcc) {
+            return res.json("Account exist!");
+        }
+
+        const account = {
+            name: req.body.name.toLowerCase(),
+            user_id: req.user._id,
+            ...req.body,
+        };
+
+        const saved = await Account.create(account);
+
+        res.status(201).json(saved);
+    } catch (error) {
+        res.status(500).json(error);
     }
-
-    const account = {
-        name: req.body.name.toLowerCase(),
-        currency: req.body.currency.toLowerCase(),
-        user_id: req.user._id,
-    };
-
-    const saved = await Account.create(account);
-
-    res.status(201).json(saved);
 };
 
 exports.updateAccount = async (req, res) => {
-    const checkAcc = await Account.findOne({
-        $and: [
-            { name: req.body.name.toLowerCase() },
-            {
-                currency: req.body.currency.toLowerCase(),
-            },
-            {
-                _id: { $ne: req.params.id },
-            },
-        ],
-    });
+    try {
+        const checkAcc = await Account.findOne({
+            $and: [
+                { name: req.body.name.toLowerCase() },
+                {
+                    currency: req.body.currency.toLowerCase(),
+                },
+                {
+                    _id: { $ne: req.params.id },
+                },
+            ],
+        });
 
-    if (checkAcc) {
-        return res.json("Account exist!");
+        if (checkAcc) {
+            return res.json("Account exist!");
+        }
+
+        const account = {
+            name: req.body.name.toLowerCase(),
+            user_id: req.user._id,
+            ...req.body,
+        };
+
+        const updated = await Account.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: account,
+            },
+            { new: true }
+        );
+
+        res.status(200).json(updated);
+    } catch (error) {
+        res.status(500).json(error);
     }
-
-    const account = {
-        name: req.body.name.toLowerCase(),
-        currency: req.body.currency.toLowerCase(),
-        user_id: req.user._id,
-    };
-
-    const updated = await Account.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set: account,
-        },
-        { new: true }
-    );
-
-    res.status(200).json(updated);
 };
 
 exports.deleteAccount = async (req, res) => {
-    await Account.findByIdAndDelete(req.params.id);
+    try {
+        await Account.findByIdAndDelete(req.params.id);
 
-    res.status(200).json("Deleted successfully!");
+        res.status(200).json("Deleted successfully!");
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
