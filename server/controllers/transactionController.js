f2c375ac2cc0c16f12f62ceff366e6801063f179
Saidabdulla <abdullamasharipov11@gmail.com>
2022-04-25 18:37:42 +0500

    EndGame

--- server/controllers/transactionController.js
+++ server/controllers/transactionController.js
@@ -1,56 +1,71 @@
 const Transaction = require("../models/transaction");
 
 exports.getTransactions = async (req, res) => {
-    const transactions = await Transaction.find({
-        $and: [
-            {
-                user_id: req.user._id,
-            },
-            { account_id: req.params.acc_id },
-        ],
-    }).populate("category_id account_id");
+    try {
+        const transactions = await Transaction.find({
+            $and: [
+                {
+                    user_id: req.user._id,
+                },
+                { account_id: req.params.acc_id },
+            ],
+        }).populate("category_id account_id");
 
-    res.status(200).json(transactions);
+        res.status(200).json(transactions);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 exports.getTransactionById = async (req, res) => {
-    const transaction = await Transaction.findById(req.params.id);
+    try {
+        const transaction = await Transaction.findById(req.params.id);
 
-    res.status(200).json(transaction);
+        res.status(200).json(transaction);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 exports.addTransaction = async (req, res) => {
-    const transaction = {
-        amount: req.body.amount,
-        isIncome: req.body.isIncome,
-        description: req.body.description,
-        note: req.body.note,
-        user_id: req.user._id,
-        account_id: req.body.account_id,
-        category_id: req.body.category_id,
-    };
+    try {
+        const transaction = {
+            ...req.body,
+            user_id: req.user._id,
+        };
 
-    const saved = await Transaction.create(transaction);
+        const saved = await Transaction.create(transaction);
 
-    res.status(201).json(saved);
+        res.status(201).json(saved);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 exports.updateTransaction = async (req, res) => {
-    const transaction = { ...req.body };
+    try {
+        const transaction = { ...req.body };
 
-    const updated = await Transaction.findByIdAndUpdate(
-        req.params.id,
-        {
-            $set: transaction,
-        },
-        { new: true }
-    );
+        const updated = await Transaction.findByIdAndUpdate(
+            req.params.id,
+            {
+                $set: transaction,
+            },
+            { new: true }
+        );
 
-    res.status(200).json(updated);
+        res.status(200).json(updated);
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
 
 exports.deleteTransaction = async (req, res) => {
-    await Transaction.findByIdAndDelete(req.params.id);
+    try {
+        await Transaction.findByIdAndDelete(req.params.id);
 
-    res.status(200).json("Deleted successfully!");
+        res.status(200).json("Deleted successfully!");
+    } catch (error) {
+        res.status(500).json(error);
+    }
 };
